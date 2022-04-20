cb7f8ceb27d861c2259a72754fcad204dc9ff4c4
nedalmuhammed <nedalmuhammed@hotmail.com>
2022-04-20 15:17:56 +0300

    Edit ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,7 +3,11 @@ import React from 'react'
 function ExpenseItem() {
     return (
         <div>
-            Expense Item !!!
+            <div>
+                March 28th 2020
+            </div>
+            <div><h2>Car Insurance </h2></div>
+            <div>$ 2887.00</div>
         </div>
     )
 }
